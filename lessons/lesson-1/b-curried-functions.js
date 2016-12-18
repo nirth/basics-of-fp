314b852bf650ec6388ace98e6252c49ed59dfe6e
David Sergey <nirth.furzahad@gmail.com>
2016-12-18 14:12:26 +0100

    Curried functions

---
+++ lessons/lesson-1/b-curried-functions.js
@@ -0,0 +1,12 @@
+console.info('---------------------');
+console.info('Intro to functional programming');
+console.info('Part 3');
+console.info('Higher Order Functions');
+console.info('---------------------');
+
+// Function `plus1` is not curried.
+const plus1 = (a, b) => a + b;
+
+// Function `plus2` is curried.
+const plus2 = (a) => (b) => a + b;
+
